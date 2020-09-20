4372bc2c1581430efb998362016dcaff1a79ebef
Squareben1 <bengittins@live.com>
2020-09-20 18:51:06 +0100

    Accounts for < 10

--- src/wave_filter.py
+++ src/wave_filter.py
@@ -1,3 +1,6 @@
 
 def wave_filter(arr):
+    for i in range(len(arr)):
+        if arr[i] < 10:
+            arr[i] = 10
     return arr
--- tests/test_wave_filter.py
+++ tests/test_wave_filter.py
@@ -1,6 +1,10 @@
 import pytest
-from src.wave_filter import *
+from src.wave_filter import wave_filter
 
 
 def test_return_arr():
     assert wave_filter([10, 11]) == [10, 11]
+
+
+def test_lower_limit():
+    assert wave_filter([9, 10]) == [10, 10]
